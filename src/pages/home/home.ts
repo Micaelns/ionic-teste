e5a2108b96eb0460991158c55ecb01bed4596733
Micael Souza <micael.souza@soledadetecnologia.com.br>
2018-08-20 00:51:24 -0300

    04-Navegação entre páginas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { Carro } from '../../app/modelos/carro';
 import { HttpErrorResponse } from '@angular/common/http';
 import { CarrosServiceProvider } from '../../providers/carros-service/carros-service';
 import { NavLifecycles } from '../../utils/ionic/nav/nav-lifecycles';
+import { EscolhaPage } from '../escolha/escolha';
 
 @Component({
   selector: 'page-home',
@@ -47,5 +48,6 @@ export class HomePage implements NavLifecycles {
 
   selecionaCarro(carro:Carro){
     console.log(carro);
+    this.navCtrl.push(EscolhaPage);
   }
 }
