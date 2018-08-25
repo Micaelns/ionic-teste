7be4ed118e49b835ac1be93cb03ad1f32537fb3e
Micael Souza <micael.ns@hotmail.com>
2018-08-25 11:41:42 -0300

    calculando acessorios

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Carro } from '../../app/modelos/carro';
+import { Acessorio } from '../../app/modelos/acessorio';
 
 @IonicPage()
 @Component({
@@ -10,13 +11,26 @@ import { Carro } from '../../app/modelos/carro';
 export class EscolhaPage {
 
   public carro: Carro;
+  public acessorios:Acessorio[];
+  private _precoTotal: number;
+
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams) {
       this.carro=this.navParams.get('carroSelecionado');
+      this._precoTotal=this.carro.preco;
+      this.acessorios=[
+        {nome: 'Freio ABS', preco: 800},
+        {nome: 'Ar-condicionado', preco: 1000},
+        {nome: 'MP3 Player', preco: 500},
+      ];
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EscolhaPage');
+  atualizaTotal(ativado:boolean, acessorio:Acessorio){
+    ativado ? this._precoTotal+= acessorio.preco:this._precoTotal-= acessorio.preco;
   }
 
+  get precoTotal(){
+    return this._precoTotal;
+  }
 }
