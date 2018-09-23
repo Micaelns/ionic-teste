7ed6d7e02fc398cbb17fdb050982af354c59549d
Micael Souza <micael.ns@hotmail.com>
2018-09-23 18:45:49 -0300

    ajuste definindo pagina principal

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, Alert } from 'ionic-angular';
 import { Carro } from '../../app/modelos/carro';
 import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -17,20 +18,19 @@ export class CadastroPage {
     public endereco:string ='';
     public email:string ='';
     public data:string = new Date().toISOString();
+
+    private _alerta : Alert;
     
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
+    private _alertCtrl: AlertController,
     private _agendamentosService: AgendamentosServiceProvider) {
         this.carro = this.navParams.get('carroSelecionado');
         this.precoTotal = this.navParams.get('precoTotal');
   }
 
   agenda(){
-    console.log(this.nome);
-    console.log(this.endereco);
-    console.log(this.email);
-    console.log(this.data);
 
     let agendamento={
       nomeCliente: this.nome,
@@ -39,11 +39,28 @@ export class CadastroPage {
       modeloCarro: this.carro.nome,
       precoTotal: this.precoTotal
     };
+    this._alerta = this._alertCtrl.create({
+      title: 'Aviso',
+      buttons:[
+        {
+          text:'OK',
+          handler: ()=>{
+            this.navCtrl.setRoot(HomePage);
+          }
+        }
+      ]
+    })
 
    this._agendamentosService.agenda(agendamento)
     .subscribe(
-      ()=>alert('Agendou!'),
-      ()=>alert('Deu problema')
+      ()=>{
+        this._alerta.setSubTitle('Agendamento realizado');
+        this._alerta.present();
+      },
+      ()=>{
+        this._alerta.setSubTitle('Falha do Agendamento! Tente novamente mais tarde');
+        this._alerta.present();
+      },
     );
   }
 }
